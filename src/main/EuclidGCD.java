8eb6364b68c403d27ae40cf904a2caa9a46717df
Samuel Biondolillo <biwinning121@gmail.com>
2017-10-10 13:31:46 -0400

    initialize repo and set up main class

---
+++ src/main/EuclidGCD.java
@@ -0,0 +1,18 @@
+/*
+ * EuclidGCD Class
+ * Samuel Biondolillo
+ * CIS210M:ZZ Data Structures and Elementary Algorithms
+ * Goal: To implement Euclid's Algorithm for finding the Greatest Common Denominator
+ * Version  0.0.1   10/10/17
+ */
+
+package main;
+
+public class EuclidGCD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
